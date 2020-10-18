8b8edde27bcebf9a209c997e592b65c7ec45505d
badawi1713 <dzaky.badawi@gmail.com>
2020-10-18 16:41:07 +0700

    Add redirect page if path is not found

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Login from "./views/pages/Login";
 import Home from "./views/pages/Home";
 import RecipeList from "./views/pages/RecipeList";
 import Setting from "./views/pages/Setting";
 
 import "./assets/styles/css/globalStyles.css";
+
+const NotFound = () => {
+  return <Redirect to="/login" />;
+};
+
 const App = () => {
   return (
     <>
@@ -16,6 +26,7 @@ const App = () => {
           <Route path="/setting" exact component={Setting} />
 
           <Route path="/login" exact component={Login} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
     </>
